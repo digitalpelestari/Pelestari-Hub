ccb6f49949de67b5c8f3bd5404a09325195446bf
LAPTOP-U2KUDEUD\PT  PELESTARI IND <digitalpelestari@gmail.com>
2026-07-02 16:52:22 +0700

    fix db

--- pelestari/lib/db.ts
+++ pelestari/lib/db.ts
@@ -1,8 +1,12 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Kosongkan jika pakai XAMPP standar
-  database: 'db_pelestari'
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
